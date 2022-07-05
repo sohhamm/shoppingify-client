b33eabb2f7f868c6a85558bb513535b39884efd5
Soham Sarkar <soham.ss1@gmail.com>
2022-07-05 15:11:12 +0530

    tailwind config + sidebar + import aliases

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react'
+import Sidebar from './Sidebar'
 
-interface MasterLayoutProps {}
-export default function MasterLayout({}: MasterLayoutProps) {
-  return <div>MasterLayout</div>
+interface LayoutProps {
+  children: JSX.Element
+}
+
+export default function Layout({children}: LayoutProps) {
+  return (
+    <div>
+      <Sidebar />
+      <div className="ml-[94px]">{children}</div>
+    </div>
+  )
 }
